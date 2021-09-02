f56565822ea62a253e3ecd3a0bf7a4ed440207ba
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-09-02 04:34:34 +0300

    reset password client side

--- client/pages/forgot-password.js
+++ client/pages/forgot-password.js
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router';
 
 const forgotPassword = () => {
   const [email, setEmail] = useState('');
-  const [success, setSuccess] = useState('');
+  const [success, setSuccess] = useState(false);
   const [code, setCode] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [loading, setLoading] = useState(false);
@@ -25,6 +25,7 @@ const forgotPassword = () => {
     if (user !== null) router.push('/');
   }, []);
 
+  // reset password (sending code)
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -38,6 +39,28 @@ const forgotPassword = () => {
     }
   };
 
+  // reset password Submitting Code
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    console.log(email, code, newPassword);
+    return;
+    try {
+      setLoading(true);
+      const { data } = await axios.post('/api/reset-password', {
+        email,
+        code,
+        newPassword,
+      });
+      setEmail('');
+      setCode('');
+      setNewPassword('');
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      toast.error(err.response.data);
+    }
+  };
+
   return (
     <>
       <h1 className="p-5 mb-4 bg-secondary text-center text-white square">
@@ -45,7 +68,7 @@ const forgotPassword = () => {
       </h1>
 
       <div className="container col-md-4 offset-md-4 pb-5">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={success ? handleResetPassword : handleSubmit}>
           <input
             type="email"
             className="form-control mb-4 p-4"
@@ -54,6 +77,28 @@ const forgotPassword = () => {
             placeholder="Enter Email"
             required
           />
+
+          {success && (
+            <>
+              <input
+                type="text"
+                className="form-control mb-4 p-4"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+                placeholder="Enter Code"
+                required
+              />
+              <input
+                type="password"
+                className="form-control mb-4 p-4"
+                value={newPassword}
+                onChange={(e) => setNewPassword(e.target.value)}
+                placeholder="Enter new password"
+                required
+              />
+            </>
+          )}
+
           <button
             type="submit"
             className="btn btn-primary w-100 p-2"
