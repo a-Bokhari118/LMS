27bcb86683907d9f5273e0c3eda3c42f746849f0
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-10-30 19:49:28 +0300

    :hammer: instructor courses list

--- client/pages/instructor/index.js
+++ client/pages/instructor/index.js
@@ -2,12 +2,15 @@ import InstructorRoute from '@components/routes/InstructorRoute';
 import axios from 'axios';
 import { Context } from '../../context';
 import { useContext, useEffect, useState } from 'react';
+import { Avatar } from 'antd';
+import Link from 'next/link';
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 
 const InstructorIndex = () => {
   const [courses, setCourses] = useState([]);
 
   useEffect(() => {
-    // loadCourses();
+    loadCourses();
   }, []);
 
   const loadCourses = async () => {
@@ -23,7 +26,62 @@ const InstructorIndex = () => {
       <div className="p-5 mb-4 bg-primary text-center square">
         Instructor Dashboard
       </div>
-      <pre>{JSON.stringify(courses, null, 4)}</pre>
+
+      {courses?.map((course) => (
+        <>
+          <div className="d-flex pt-2 align-items-center ">
+            <div style={{ marginRight: '15px' }}>
+              <Avatar
+                size={80}
+                src={course.image ? course.image.Location : '/coures.png'}
+              />
+            </div>
+
+            <div className=" pl-2 " style={{ width: '100%' }}>
+              <div className="row d-flex ">
+                <div className="col ">
+                  <div className="d-flex flex-column  pl-3 ">
+                    <Link
+                      href={`/instructor/course/view/${course._id}`}
+                      className="pointer"
+                    >
+                      <a className="mt-2 text-primary">
+                        <h5>{course.name}</h5>
+                      </a>
+                    </Link>
+                    <p>
+                      <b>{course.lessons.length} Lessons</b>
+                    </p>
+
+                    {course.lessons.length < 5 ? (
+                      <p className="text-warning">
+                        At least 5 lessons are required to publish a course
+                      </p>
+                    ) : course.published ? (
+                      <p className="text-success">Your course is Live!</p>
+                    ) : (
+                      <p className="text-success">
+                        Your course is ready to be published
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="col-md-3 mt-3 justify-content-end ">
+                  {course.published ? (
+                    <div>
+                      <CheckCircleOutlined className="h5 pointer text-success" />
+                    </div>
+                  ) : (
+                    <div>
+                      <CloseCircleOutlined className="h5 pointer text-warning" />
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      ))}
     </InstructorRoute>
   );
 };
