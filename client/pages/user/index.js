66e780eb91fa5ddbf144ef9a00614c84be92458f
Abdulrhman Bokhari <a.bokhari118@hotmail.com>
2021-11-02 11:59:15 +0300

    :hammer: user courses in dashboad

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,16 +1,33 @@
 import axios from 'axios';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Context } from '../../context';
 import UserRoute from '@components/routes/UserRoute';
+import { toast } from 'react-toastify';
 
 const UserIndex = () => {
   const {
     state: { user },
   } = useContext(Context);
 
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    loadCourses();
+  }, []);
+
+  const loadCourses = async () => {
+    try {
+      const { data } = await axios.get(`/api/user-courses`);
+      setCourses(data);
+    } catch (err) {
+      console.log(err);
+      toast.error('Loading courses faild');
+    }
+  };
   return (
     <UserRoute>
       <h1 className="p-5 mb-4 bg-light text-center square">User Dashboard</h1>
+      <pre>{JSON.stringify(courses, null, 4)}</pre>
     </UserRoute>
   );
 };
